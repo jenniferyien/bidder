20ca4bead109fc4fa8bbfc5eee51d998ac95bb75
Jennifer Yien <jenniferllwu@gmail.com>
2015-11-30 14:36:59 -0500

    jquery and boiler plate for mondgo and html

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var mongo = require('mongodb');
+var client = mongo.MongoClient;
+
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
+
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
 
 
 app.use(express.static(__dirname + '/public'));
@@ -9,8 +17,6 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
   res.sendfile('index.html')
 });
-
-
 
 http.listen(3000, function(){
   console.log("listening on 3000");
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Bidder
+
+Technologies Used:
+* MongoDB
+* Node.js w/Express
+* Angular
+
+
+Collaboration between Jaskar Singh & Jennifer Yien 
