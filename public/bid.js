afdd782a4c8a62cf83e598b307d1f6705ac372a6
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-12-01 15:27:37 -0500

    not important

--- public/bid.js
+++ public/bid.js
@@ -5,15 +5,15 @@ $(function(){
     event.preventDefault();
 
     $.ajax({
-      url: 'https://open.api.ebay.com/shopping?',
+      url: "http://open.api.ebay.com/shopping?callname=FindPopularItems&responseencoding=XML&appid=Nonec795f-fb4b-4bcf-89a8-358c2f1d592&siteid=0&QueryKeywords=dog&version=713",
       method: 'GET',
-      dataType: "json",
+      dataType: "script",
       data: {
-          callname:'FindPopularItems',
-          appid: ENV["EBAY_ID"],
-          version:517,
-          siteid:0,
-          responseencoding:JSON
+          // callname:'FindPopularItems',
+          // appid: "Nonec795f-fb4b-4bcf-89a8-358c2f1d592",
+          // version:517,
+          // siteid:0,
+          responseencoding:NV
       },
       success: function(data, success, xhr){
         console.log("data is " +data);
