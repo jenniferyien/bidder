232536dd0575f5d804836767007e01303ec474af
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-12-01 14:59:45 -0500

    api work

--- public/bid.js
+++ public/bid.js
@@ -1,5 +1,28 @@
 $(function(){
-  var socket = io();
+  // var socket = io();
 
+  $("#submit").click(function(event){
+    event.preventDefault();
+
+    $.ajax({
+      url: 'https://open.api.ebay.com/shopping?',
+      method: 'GET',
+      dataType: "json",
+      data: {
+          callname:'FindPopularItems',
+          appid: ENV["EBAY_ID"],
+          version:517,
+          siteid:0,
+          responseencoding:JSON
+      },
+      success: function(data, success, xhr){
+        console.log("data is " +data);
+      },
+      error: function(xhr, data, error){
+        console.log("error is "+error);
+      }
+
+    });
+  });
 
 }); //closing function
