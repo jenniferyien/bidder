f79bccf89c93a35061477655f15865c0fd2bb0dc
Jennifer Yien <jenniferllwu@gmail.com>
2015-12-01 15:44:09 -0500

    ebay api data retr

--- public/bid.js
+++ public/bid.js
@@ -5,23 +5,24 @@ $(function(){
     event.preventDefault();
 
     $.ajax({
-      url: 'http://open.api.ebay.com/shopping?callname=FindProducts',
+      url: 'http://open.api.ebay.com/shopping?callname=FindPopularItems',
       method: 'GET',
       dataType: "jsonp",
       jsonp: "callbackname",
       crossDomain : true,
       data: {
-          // callname:'FindPopularItems',
-          QueryKeywords : 'dog',
+          // appid: "",
+          QueryKeywords : 'harry',
+          MaxEntries:20,
           version:517,
           siteid:0,
           responseencoding: 'JSON'
       },
       success: function(data, success, xhr){
-        console.log("data is " +data);
+        console.log("data is", data);
       },
       error: function(xhr, data, error){
-        console.log("error is "+error);
+        console.log("error is", error);
       }
 
     });
