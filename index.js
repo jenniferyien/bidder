835b9e05ec977258d36c3ddb696a954297689887
Jennifer Yien <jenniferllwu@gmail.com>
2015-12-02 13:59:43 -0500

    install bodyparser

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-// var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var path = require('path');
 var mongo = require('mongodb');
 var client = mongo.MongoClient;
@@ -8,8 +8,8 @@ var client = mongo.MongoClient;
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
 
-// app.use(bodyParser.urlencoded({extend: false}));
-// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
