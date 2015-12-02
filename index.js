81d2ca07c94b47beaf8f9a50ee627cdbd041bfed
Jennifer Yien <jenniferllwu@gmail.com>
2015-12-02 13:35:48 -0500

    mongodb user connection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var path = require('path');
 var mongo = require('mongodb');
 var client = mongo.MongoClient;
@@ -8,11 +8,29 @@ var client = mongo.MongoClient;
 var http = require('http').Server(app);
 var io = require("socket.io")(http);
 
-app.use(bodyParser.urlencoded({extend: false}));
-app.use(bodyParser.json());
-
+// app.use(bodyParser.urlencoded({extend: false}));
+// app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
+
+//mongo
+client.connect('mongodb://localhost:27017/bidder', function(error, db){
+  if (error) {console.log("Coundn't connect to database")}
+  app.get('/users', function(req, res){
+    db.collection('user').find().toArray(function(error, users){
+      res.send(users);
+    })
+  }); //get user list for index /show
+
+  app.post('/users/create', function(req, res){
+    db.collection('user').insert(req.body, function(error){
+      if (error) {res.send(error)}
+    });
+  }); //new and create
+
+});
+
+
 
 app.get('/', function(req, res){
   res.sendfile('index.html')
