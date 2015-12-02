4dcfc761a3df07f349dc10f23e2181b37fc8966b
Jaskaranjit Singh <singhjaskar1@gmail.com>
2015-12-02 13:51:18 -0500

    form for user

--- public/bid.js
+++ public/bid.js
@@ -3,7 +3,6 @@ $(function(){
 
   $("#submit").click(function(event){
     event.preventDefault();
-
     $.ajax({
       url: 'http://open.api.ebay.com/shopping?callname=FindPopularItems',
       method: 'GET',
@@ -18,14 +17,37 @@ $(function(){
           siteid:0,
           responseencoding: 'JSON'
       },
-      success: function(data, success, xhr){
+      success: function(data, status, xhr){
         console.log("data is", data);
       },
-      error: function(xhr, data, error){
+      error: function(xhr, status, error){
         console.log("error is", error);
       }
-
     });
   });
+
+  $("#registering").click(function(event){
+    event.preventDefault();
+    var username = $("#name").val();
+    var userEmail = $("#email").val();
+    var userPassword = $("#password").val();
+    //created hash for ajax call
+    var hash = {name: username, email: userEmail, password: userPassword};
+    $.ajax({
+      url: '/users/create',
+      method: 'POST',
+      data: hash,
+      success: function(data, status, xhr){
+        console.log(data);
+      },
+      error: function(xhr, status, error){
+        console.log("error is ", error);
+      },
+    });
+
+
+  })
+
+
 
 }); //closing function
